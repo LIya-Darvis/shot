fe9812edebe137c94c17f92157e9fa4d20dbf853
Liya Darvis <anastasiaadm2019@gmail.com>
2025-10-23 22:35:42 +0300

    features: добавлены псевдодействия оппонента

--- src/backend/models/functions.ts
+++ src/backend/models/functions.ts
@@ -1,9 +1,14 @@
 import { TMember, TWeapon } from './types';
 
+
+/**
+ * выстрел  
+ * (с учетом двойного урона)
+ */
 export const makeShot = (opponent: TMember, weapon: TWeapon): TMember => {
     return {
         ...opponent,
-        health: opponent.health - (weapon.isDoubleDmg ? 2 : 1)
+        health: opponent.health - (weapon.isDoubleDmg ? weapon.charges.pop() * 2 : weapon.charges.pop())
     }
 }
 
@@ -11,42 +16,54 @@ export const makeShot = (opponent: TMember, weapon: TWeapon): TMember => {
  * выброс следующего патрона 
  * (для предмета "сигареты")
  */
-export const dropCharge = (weapon: TWeapon): void => {
-    weapon.charges.splice(0, 1);
+export const dropCharge = (weapon: TWeapon): TWeapon => {
+    weapon.charges.pop()
+    return {
+        ...weapon,
+    };
 }
 
 /**
  * узнать какой патрон следующий
  * (для предмета "батарейка для рации")
  */
-export const chargeMessage = (weapon: TWeapon): void => {
+export const chargeMessage = (weapon: TWeapon): number => {
     console.log("Следующий патрон:", weapon.charges[0]);
-    weapon.charges[0];
+    return weapon.charges[weapon.charges.length - 1];
 }
 
 /**
- * хилл/анлак (рандом)
+ * хилл/анлак (+2 или -1)
  * (для предмета "зараженная вода")
  */
-export const drinkWater = (currentPlayer: TMember): void => {
+export const drinkWater = (currentPlayer: TMember): TMember => {
     const healthPoints = [-1, 2];
-    currentPlayer.health += healthPoints[Math.floor(Math.random() * 2)];
+    return {
+        ...currentPlayer,
+        health: currentPlayer.health += healthPoints[Math.floor(Math.random() * 2)]
+    }
 }
 
 /**
- * обычный хилл
+ * обычный хилл + 1
  * (для предмета "бинты")
  */
-export const healthUp = (currentPlayer: TMember): void => {
-    currentPlayer.health += 1;
+export const healthUp = (currentPlayer: TMember): TMember => {
+    return {
+        ...currentPlayer,
+        health: currentPlayer.health += 1
+    };
 }
 
 /**
  * дабл дамаг на оружие
  * (для предмета "ножовка)
  */
-export const useKnife = (weapon: TWeapon): void => {
-    weapon.isDoubleDmg = true 
+export const useKnife = (weapon: TWeapon): TWeapon => {
+    return {
+        ...weapon,
+        isDoubleDmg: true
+    }
 }
 
 /**
--- src/frontend/pages/main/mainPage.tsx
+++ src/frontend/pages/main/mainPage.tsx
@@ -1,42 +1,86 @@
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TMember, TWeapon } from '../../../backend/models/types'
+import { chargeMessage, drinkWater, dropCharge, healthUp, makeShot, useHandcuffs, useKnife } from '../../../backend/models/functions';
 import './styles.css';
-import { makeShot, useHandcuffs } from '../../../backend/models/functions';
 
 export const MainPage = () => {
-    const initOpponent: TMember = { health: 2, inventory: [], isSkipping: false, maxHealth: 4, name: 'Диллер' };
-    const currentPlayer: TMember = { health: 2, inventory: [], isSkipping: false, maxHealth: 4, name: 'Игрок' };
-    const initWeapon: TWeapon = { charges: [0, 1, 0, 1, 1], isDoubleDmg: false }
+    const initOpponent: TMember = { health: 6, inventory: [], isSkipping: false, maxHealth: 10, name: 'Диллер' };
+    const initPlayer: TMember = { health: 6, inventory: [], isSkipping: false, maxHealth: 10, name: 'Игрок' };
+    const initWeapon: TWeapon = { charges: [0, 1, 0, 1, 0, 1, 0, 1], isDoubleDmg: false }
 
+    const [player, setPlayer] = useState<TMember>(initPlayer);
     const [opponent, setOpponent] = useState<TMember>(initOpponent);
-    const [player, setPlayer] = useState<TMember>(currentPlayer);
     const [weapon, setWeapon] = useState<TWeapon>(initWeapon);
+    const [isWaiting, setIsWaiting] = useState<boolean>(false);
 
-    const healthRef = useRef(opponent.health);
-
+    console.log(player);
     console.log(opponent);
+    console.log(weapon);
 
     // ToDo:
-    // - добавить кнопки на остальные предметы (игроку)
+    // ✔ добавить кнопки на остальные предметы (игроку)
+    // ✔ реализовать псевдодействия оппонента
     // - добавить генерацию инвентаря
-    // - реализовать псевдодействия оппонента
+    // - поправить сброс наручников после двух выстрелов подряд
+    // - добавить выстрел в себя
+
+    const stupidOpponent = () => {
+        setOpponent(healthUp(opponent));
+        setTimeout(() => { }, 2000);
+
+        setPlayer(makeShot(player, weapon))
+        // сбрасываем дабл урон (на всякий)
+        setWeapon({ ...weapon, isDoubleDmg: false })
+        setTimeout(() => { }, 2000);
+
+        // сбрасываем наручники (на всякий)
+        setPlayer({ ...player, isSkipping: false });
+        setTimeout(() => { }, 2000);
+    }
+
+    useEffect(() => {
+        if (isWaiting) {
+            stupidOpponent();
+            setTimeout(() => { }, 1000);
+            // проверка на наручники игрока
+            // if (player.isSkipping) {
+            //     console.log('> *анимация проверки наручников на игроке*');
+            //     setTimeout(() => { }, 2000);
+            //     stupidOpponent();
+            // }
+            setIsWaiting(false);
+        }
+
+    }, [isWaiting])
+
 
     return (
         <div className='main-container'>
             <div className='head-container'>
-                <div className='member-info'>{player.name} - [{healthRef.current}]</div>
+                <div className='member-info'>{player.name} - [{player.health}]</div>
                 {/* <div className='round-info'>Round {data.round || 1}</div> */}
                 <div className='member-info'>{opponent.name} - [{opponent.health}]</div>
             </div>
             <div className='game-container'>
-                <div className=''>
-                    <button onClick={() => setOpponent(makeShot(opponent, weapon))}>Выстрел!</button>
-                    <button onClick={() => setOpponent(useHandcuffs(opponent))}>Повесить наручники</button>
-                </div>
+                <button disabled={isWaiting} onClick={() => {
+                    setOpponent(makeShot(opponent, weapon))
+                    // сбрасываем дабл урон (на всякий)
+                    setWeapon({ ...weapon, isDoubleDmg: false })
+                    setIsWaiting(!opponent.isSkipping);
+                }
+                }>Выстрел!</button>
+                <button disabled={isWaiting} onClick={() => setOpponent(useHandcuffs(opponent))}>Повесить наручники</button>
+                <button disabled={isWaiting} onClick={() => setWeapon(dropCharge(weapon))}>Покурить</button>
+                <button disabled={isWaiting} onClick={() => chargeMessage(weapon)}>Рация</button>
+                <button disabled={isWaiting} onClick={() => setPlayer(drinkWater(player))}>Вода</button>
+                <button disabled={isWaiting} onClick={() => setPlayer(healthUp(player))}>Бинты</button><button disabled={isWaiting} onClick={() => setWeapon(useKnife(weapon))}>Ножовка</button>
             </div>
         </div>
     )
 }
+
+
+
 
 // export const Button = (props) => {
 //     return (
